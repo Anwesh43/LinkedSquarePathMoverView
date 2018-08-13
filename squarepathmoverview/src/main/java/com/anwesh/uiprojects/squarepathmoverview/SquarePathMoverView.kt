29efd01827f3420c6154e50b0717f70fe5c034e3
Anwesh43 <anweshthecool0@gmail.com>
2018-08-14 01:07:19 +0530

    canvas extension drawSPMNode function

--- squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
+++ squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
@@ -9,9 +9,26 @@ import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.RectF
 
 val nodes : Int = 5
+
+fun Canvas.drawSPMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val sc1 : Float = Math.min(0.5f, scale)
+    val sc2 : Float = Math.min(0.5f, scale)
+    val gap : Float = w / nodes
+    val size : Float = gap / 3
+    val x1 = gap * sc2
+    val x2 = gap * sc1
+    paint.color = Color.parseColor("#66BB6A")
+    save()
+    translate(i * gap + gap/2, h/2)
+    drawRect(RectF(-size/2 + x1, -size/2, size/2 + x2, size/2), paint)
+    restore()
+}
 
 class SquarePathMoverView(ctx : Context) : View(ctx) {
 

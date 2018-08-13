884e3cb532460d6f473d72e69838c835183145a2
Anwesh43 <anweshthecool0@gmail.com>
2018-08-14 02:13:23 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarepathmoveview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarepathmoveview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarepathmoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarepathmoverview.SquarePathMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquarePathMoverView.create(this)
     }
 }

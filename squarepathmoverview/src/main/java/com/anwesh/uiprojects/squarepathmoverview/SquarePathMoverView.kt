68688278e4eeb136535e0e8f97d069c38293ff83
Anwesh43 <anweshthecool0@gmail.com>
2018-08-14 00:41:15 +0530

    importing libraries and defining nodes

--- squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
+++ squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.squarepathmoverview
 /**
  * Created by anweshmishra on 14/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val nodes : Int = 5 

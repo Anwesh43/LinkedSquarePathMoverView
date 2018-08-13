8ea03b74579182304f2b53638a2fe44870163447
Anwesh43 <anweshthecool0@gmail.com>
2018-08-14 00:44:56 +0530

    adding SquarePathMoverView class

--- squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
+++ squarepathmoverview/src/main/java/com/anwesh/uiprojects/squarepathmoverview/SquarePathMoverView.kt
@@ -11,4 +11,22 @@ import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.RectF
 
-val nodes : Int = 5 
+val nodes : Int = 5
+
+class SquarePathMoverView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
